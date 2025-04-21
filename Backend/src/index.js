607187bb0637ea76e85e113d9774a062cb65d74c
sourav-maji <s722143@gmail.com>
2025-04-21 20:36:10 +0530

    Basic express server set up is done

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,12 +1,18 @@
 import express from "express"
+import dotenv from "dotenv"
+
+
+dotenv.config({path:"./.env"})
 
 const app = express()
+
+const PORT = process.env.PORT || 8000
 
 app.get("/", (req,res)=>{
     res.json({"message" : "Server is Up and running"})
 })
 
-app.listen(8000, (req,res)=>{
-    console.log(`Server is running on port 8000`);
+app.listen(PORT, (req,res)=>{
+    console.log(`Server is running on port ${PORT}`);
     
 })
